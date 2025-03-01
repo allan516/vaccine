b17d37f0b6e7da8e0126fc63be146fabfcb7b87b
Allan <allan.mendes0976@gmail.com>
2025-03-01 11:56:56 -0300

    feat: create routes and controllers for the vaccine

---
+++ src/controllers/vaccine-controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+
+// router.post("/pet/:id/vaccine", vaccineController.postVaccine);
+// router.delete("/pet/:id/vaccine/:vaccineName", vaccineController.deleteVaccine);
+// router.patch("/pet/:id/vaccine/:vaccineName", vaccineController.updateVaccine);
+// router.get("/pet/:id/vaccine", vaccineController.getVaccine);
+
+export const postVaccine = async (req: Request, res: Response) => {};
+
+export const getVaccine = async (req: Request, res: Response) => {};
+
+export const updateVaccine = async (req: Request, res: Response) => {};
+
+export const deleteVaccine = async (req: Request, res: Response) => {};
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { login } from "./controllers/login-controller";
 import * as petController from "./controllers/pet-controller";
+import * as vaccineController from "./controllers/vaccine-controller";
 
 const router = Router();
 
@@ -9,5 +10,10 @@ router.post("/pet", petController.postPet);
 router.delete("/pet/:id", petController.deletePet);
 router.patch("/pet/:id", petController.updatePet);
 router.get("/pet", petController.getPet);
+
+router.post("/pet/:id/vaccine", vaccineController.postVaccine);
+router.delete("/pet/:id/vaccine/:vaccineName", vaccineController.deleteVaccine);
+router.patch("/pet/:id/vaccine/:vaccineName", vaccineController.updateVaccine);
+router.get("/pet/:id/vaccine", vaccineController.getVaccine);
 
 export default router;
