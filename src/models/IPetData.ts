5749c37f95a3101a4361ff0fdc9085ca3c49348a
Allan <allan.mendes0976@gmail.com>
2025-04-23 23:39:45 -0300

    feat: update interface IpetData and IpetVaccine

--- src/models/IPetData.ts
+++ src/models/IPetData.ts
@@ -1,6 +1,8 @@
 export interface IPetData {
   name: string;
   age: number;
+  category: string;
+  breed: string;
   vaccines: IpetVaccine[];
 }
 
@@ -8,4 +10,12 @@ export interface IpetVaccine {
   id: string;
   name: string;
   date: Date;
+  status: VaccineStatus;
+}
+
+export enum VaccineStatus {
+  PENDING = "pending",
+  APPLIED = "applied",
+  EXPIRED = "expired",
+  MISSED = "missed",
 }
