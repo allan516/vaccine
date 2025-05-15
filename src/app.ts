20acadd495e745cb91d9c9fd2251a371c8a59460
Allan <allan.mendes0976@gmail.com>
2025-05-15 18:37:23 -0300

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,18 @@ import cors from "cors";
 
 function createApp() {
   const app = express();
+
   app.use(
     cors({
       origin: "https://vaccinecontrol.vercel.app",
     })
   );
+
+  app.options("*", cors());
+
   app.use(express.json());
   app.use("/", router);
+
   return app;
 }
 
